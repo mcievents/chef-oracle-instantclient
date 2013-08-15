656a0cee4d111fd88b38dbd2d4c746a0b87eb9b1
Jonathan Trull <jtrull@wyndhamjade.com>
2013-08-15 11:18:51 -0500

    Point ld.so.conf.d entry to correct directory

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,7 @@
 default['oracle_instantclient']['version'] = '11.2.0.3.0'
 default['oracle_instantclient']['download_base_url'] = 'http://CHANGEME'
 default['oracle_instantclient']['install_dir'] = '/opt/oracle'
+default['oracle_instantclient']['product_subdir'] = 'instantclient_11_2'
 default['oracle_instantclient']['components'] = {
   'basic' => true,
   'basiclite' => false,
