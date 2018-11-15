0ccbb4f2b9c8e655d3cab583987406c8a8dc7b5d
Jonathan Trull <1119601+jtrull@users.noreply.github.com>
2018-11-15 16:50:47 -0600

    Update copyright and bump version

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,8 @@
 name             'oracle-instantclient'
-maintainer       'Wyndham Jade LLC'
-maintainer_email 'itsupport@wyndhamjade.com'
+maintainer       'MCI USA'
 license          'Apache 2.0'
 description      'Installs/Configures oracle-instantclient'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.2.0'
 
 depends 'apt'
